1263da3882ca750cfbaeab08b1cbdede0263c854
Fahad <fahad05munir@gmail.com>
2026-09-14 11:21:37 +0500

    code updated!

---
+++ Models/practiceUserModel.js
@@ -0,0 +1,24 @@
+let mongoose = require("mongoose");
+
+let userSchema = mongoose.Schema({
+
+    userName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+
+});
+
+let userModel = mongoose.model("userModel" , userSchema);
+
+module.exports = userModel;

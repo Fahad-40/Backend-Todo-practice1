6358057d4b8aad2e2b421366809434f77969d813
Fahad <fahad05munir@gmail.com>
2026-09-14 11:37:11 +0500

    code updated!

--- Validator/practiceSignupValidator.js
+++ Validator/practiceSignupValidator.js
@@ -1,14 +1,15 @@
-const Joi = require("joi");
+let Joi = require("joi");
 
-const signUpSchema = Joi.object({
+let signUpSchema = Joi.object({
+
     userName: Joi.string().required(),
-    email: Joi.string().required(),
+    password: Joi.string().required(),
     password: Joi.string()
     .min(8)
     .max(64)
     .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])'))
     .required()
-});
+
+})
 
 module.exports = signUpSchema;
-
